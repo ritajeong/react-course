aae32a788e188a3d7508488b6472b58d2cec025f
ritajeong <ritajeong.dev@gmail.com>
2022-08-02 22:59:11 +0900

    Modal 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Backdrop from './components/Backdrop.js';
 import Todo from './components/Todo.js'
+import Modal from './components/Modal.js'
+
 function App() {
   return <div>
     <h1>My todos</h1>
     <Todo text='Learn React'/>
     <Todo text='Eat bread'/>
-    <Todo text='Go workout'/>
+    <Todo text='Go workout' />
+    <Modal />
+    <Backdrop />
   </div>;
 }
 
