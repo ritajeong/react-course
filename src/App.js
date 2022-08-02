17ec72122347ac42ef02b1a76176abf38487c549
ritajeong <ritajeong.dev@gmail.com>
2022-08-02 22:44:17 +0900

    Todo Component 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Todo from './components/Todo.js'
 function App() {
-  return <div>Hello!</div>;
+  return <div>
+    <h1>My todos</h1>
+    <Todo />
+    <Todo />
+    <Todo />
+  </div>;
 }
+
 
 export default App;
