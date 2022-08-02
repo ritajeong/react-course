2969350c9b3f362cf72c270261e4f39008ae0ff0
ritajeong <ritajeong.dev@gmail.com>
2022-08-02 23:24:12 +0900

    기능을 props로 전달하기

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,14 +8,18 @@ function Todo(props) {
   function deleteHandler() {
     setModalIsOpen(true);
   }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
   return (
     <div className='card'>
       <h2>{props.text}</h2>
       <div className='actions'>
         <button className='btn' onClick={deleteHandler}>Delete</button>
       </div>
-      { modalIsOpen && <Modal /> }
-      { modalIsOpen && <Backdrop /> }
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={ closeModalHandler } /> }
+      {modalIsOpen && <Backdrop onClick={ closeModalHandler } /> }
     </div>
   );
 }
