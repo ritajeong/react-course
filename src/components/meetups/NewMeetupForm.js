c71b4fc5547b6994a775576d0a0d58a62aeeb5af
ritajeong <ritajeong.dev@gmail.com>
2022-08-03 21:17:06 +0900

    사용자 입력 받기 및 양식 제출 처리하기
    폼 제출, submission 처리, 브라우저 기본 동작 차단, 사용자 입력값 가져오기

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,30 +1,54 @@
-import Card from '../ui/Card'
-import classes from './NewMeetupForm.module.css'
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
 
 function NewMeetupForm() {
-  return <Card>
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='title'>Meetup Title</label>
-        <input type='text' required id='title' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='image'>Meetup Image</label>
-        <input type='url' required id='image' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='address'>Address</label>
-        <input type='text' required id='address' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='description'>Meetup Description</label>
-        <textarea type='descriprion' required id='description' rows='5' />
-      </div>
-      <div className={classes.actions}>
-        <button>Add Meetup</button>
-      </div>
-    </form>
-  </Card>
+  const titleInputRef = useRef(); //참조 객체가 생성됨
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+  function submitHandler(event) {
+    event.preventDefault(); //vanila
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription
+    }
+
+    console.log(meetupData);
+  }
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={imageInputRef}/>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea type="descriprion" required id="description" rows="5" ref={descriptionInputRef} />
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default NewMeetupForm;
