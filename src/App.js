8ea55842fe3c4b12f3e0e510453e06b7c2056ab0
ritajeong <ritajeong.dev@gmail.com>
2022-08-03 18:06:46 +0900

    라우팅 추가하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route} from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Routes>
+      <Route path='/' exact element={<AllMeetupsPage/>} />
+      <Route path='/new-meetup' element={<NewMeetupPage/>} />
+      <Route path='/favorites' element={<FavoritesPage/>} />
+    </Routes>
+  </div>;
 }
 
 export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+function NewMeetupPage() {
+  return <div>newMeetup Page</div>;
+}
+
+export default NewMeetupPage;
