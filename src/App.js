ef7d27c7b677c788330e85c6cc2ae630f1dc4fa1
ritajeong <ritajeong.dev@gmail.com>
2022-08-03 20:37:59 +0900

    props.children을 사용해서 wrapper 컴포넌트(layout) 생성

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { Routes, Route} from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
-
+import Layout from './components/layout/Layout'
 function App() {
   return <div id='app'>
-    <MainNavigation/>
+    <Layout>
       <Routes>
         <Route path='/' exact element={<AllMeetupsPage/>} />
         <Route path='/new-meetup' element={<NewMeetupPage/>} />
         <Route path='/favorites' element={<FavoritesPage/>} />
       </Routes>
+    </Layout>
   </div>;
 }
 
