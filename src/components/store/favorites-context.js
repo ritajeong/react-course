f4f089bcbec42c2d68769218ec71261a8061dffa
ritajeong <ritajeong.dev@gmail.com>
2022-08-04 19:26:47 +0900

    Context 논리 및 State 업데이트의 다양한 방법

--- src/components/store/favorites-context.js
+++ src/components/store/favorites-context.js
@@ -7,6 +7,26 @@ const FavoritesContext = createContext({
 
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    // setUserFavorites(userFavorites.concat(favoriteMeetup)); //concat은  favorite이 추가된 배열을 새로 반환함
+    // 위처럼 쓰면 안되는 이유 :  useState를 사용할 때, 리액트가 state 업데이트를 즉시 처리하지 ㅇ낳고, 작업 예약을 걸어 뒤로 미룬다.
+    // 꽤 빠른 시점에 처리되긴 하지만 즉시처리는 아님. 따라서 최신 상태 스냅샷에 따라 업데이트한다고 하면, 상태 스냅샷이 최신상태를 반영하지 못해 상태 업데이트가 불가능한 경우가 생긴다.
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+     }); //상태 업데이트 함수를 대신 호출할 수 있는 대체 양식을 쓴다. 새 값을 전달하는 대신, 상태 업데이트 함수에 직접 함수를 전달!
+  }
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+    })
+  }
+  
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
+
+
   const context = { //컴포넌트에 전달해야하는 최신값을 보유하고 있음(value context로 전달하는)
     favorites: userFavorites, //context의 초기값에도 정의했기 때문에 Favorites의 키값을 가지고 있음. userFavorites 배열을 state값으로 설정함.
     totalFavorites: userFavorites.length
