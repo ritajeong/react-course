441abc39b872b14825ca7f07903d72e7b822d9f5
ritajeong <ritajeong.dev@gmail.com>
2022-08-04 15:17:56 +0900

    POST 요청 보내기

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,7 +2,14 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
   function addMeetupHandler(meetupData) {
-    
+    fetch('https://react-course-472b5-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }); //firebase 특성. url 뒤에 json 붙임. 두번째 인자는 디폴트가 get이고 명시할 수 있음
   }
   return <div>
     <h1>Add New Meetup</h1>
